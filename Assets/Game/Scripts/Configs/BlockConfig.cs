91b045de612057f741a8f663705ec4d4807e6f49
SabadashDenis <denissabadash909@gmail.com>
2024-10-22 22:37:08 +0300

    fix: build failures

--- Assets/Game/Scripts/Configs/BlockConfig.cs
+++ Assets/Game/Scripts/Configs/BlockConfig.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Game.Scripts.Data;
-using UnityEditor.Animations;
 using UnityEngine;
 
 namespace Game.Scripts.Core
@@ -20,9 +19,9 @@ namespace Game.Scripts.Core
     public struct BlockConfigData
     {
         [SerializeField] private BlockType type;
-        [SerializeField] private AnimatorController animatorController;
+        [SerializeField] private RuntimeAnimatorController animatorController;
 
         public BlockType Type => type;
-        public AnimatorController AnimatorController => animatorController;
+        public RuntimeAnimatorController AnimatorController => animatorController;
     }
 }
